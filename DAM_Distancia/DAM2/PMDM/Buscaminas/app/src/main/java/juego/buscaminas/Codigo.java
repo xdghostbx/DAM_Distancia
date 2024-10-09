d4ec818e3cef73f6689450cddf6348db5acda902
Daniel <jodamarmar@gmail.com>
2024-10-09 11:40:47 +0200

    update

--- DAM_Distancia/DAM2/PMDM/Buscaminas/app/src/main/java/juego/buscaminas/Codigo.java
+++ DAM_Distancia/DAM2/PMDM/Buscaminas/app/src/main/java/juego/buscaminas/Codigo.java
@@ -9,8 +9,15 @@ public class Codigo {
         Random aleatorio = new Random();
         // Generamos un array
         int[][] cuadricula = new int[5][5];
+
+        //Se inicia toda la cuadricula en 0
+        for(int a = 0; a < 5; a++){
+            for(int b = 0; b < 5; b++){
+                cuadricula[a][b] = 0;
+            }
+        }
         // Con un bucle for ponemos 5 "minas"
-        for (int a = 0; a < 5; a++) {
+        for (int a = 0; a < 3; a++) {
             int i = aleatorio.nextInt(5);
             int j = aleatorio.nextInt(5);
             // Comprueba si ya hay una mina en la posición
--- DAM_Distancia/DAM2/PMDM/Buscaminas/app/src/main/java/juego/buscaminas/MainActivity.java
+++ DAM_Distancia/DAM2/PMDM/Buscaminas/app/src/main/java/juego/buscaminas/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
 
     private int[][] cuadricula;
     private Button[][] botones;
+    private int contador = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,17 +29,22 @@ public class MainActivity extends AppCompatActivity {
                 botones[i][j].setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        if (cuadricula[finalI][finalJ] == 1) {
-                            v.setBackgroundColor(getResources().getColor(android.R.color.holo_red_dark));
-                            mostrarAlertaPerdido();
-                        } else {
-                            v.setBackgroundColor(getResources().getColor(android.R.color.holo_green_dark));
+                            if (cuadricula[finalI][finalJ] == 1) {
+                                v.setBackgroundColor(getResources().getColor(android.R.color.holo_red_dark));
+                                mostrarAlertaPerdido();
+                            } else{
+                                contador++;
+                                v.setBackgroundColor(getResources().getColor(android.R.color.holo_green_dark));
+                                comprobarArea(finalI, finalJ);
+                                if (contador == 22){
+                                    mostrarAlertaGanado();
+                                }
+                            }
                         }
-                    }
                 });
             }
         }
-
+        reiniciarColores();
         Button btJugar = findViewById(R.id.btJugar);
         btJugar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,6 +54,14 @@ public class MainActivity extends AppCompatActivity {
                 // Generar una nueva cuadrícula
                 cuadricula = Codigo.cuadricula(MainActivity.this);
             }
+        });
+
+        Button btTutorial = findViewById(R.id.btTutorial);
+        btTutorial.setOnClickListener(new View.OnClickListener(){
+          @Override
+          public void onClick(View v){
+              tutorial();
+          }
         });
     }
 
@@ -60,7 +74,29 @@ public class MainActivity extends AppCompatActivity {
                 .show();
     }
 
+    private void mostrarAlertaGanado() {
+        new AlertDialog.Builder(this)
+                .setTitle("¡Enhorabuena!")
+                .setMessage("Has ganado.")
+                .setPositiveButton(android.R.string.ok, (dialog, which) -> reiniciarColores())
+                .setCancelable(false)
+                .show();
+    }
+
+    private void tutorial(){
+        new AlertDialog.Builder(this)
+                .setTitle("Buscaminas")
+                .setMessage("El objetivo del juego es no tocar las minas."+"\nColores:"+"\n    -Verde: " +
+                        "No hay mina en radio 1 desde \n    la casilla."+"\n    -Naranja: Hay mina en radio 1 " +
+                        "desde \n    la casilla."+"\n    -Naranja Oscuro: Hay 2 minas en radio \n    1 desde la casilla"+
+                        "\n    -Rojo: Has tocado una mina.")
+                .setPositiveButton(android.R.string.ok, (dialog, which) -> {})
+                .setCancelable(false)
+                .show();
+    }
+
     private void reiniciarColores() {
+        contador = 0;
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
                 botones[i][j].setBackgroundColor(getResources().getColor(android.R.color.darker_gray));
@@ -68,5 +104,44 @@ public class MainActivity extends AppCompatActivity {
         }
         // Generar una nueva cuadrícula
         cuadricula = Codigo.cuadricula(MainActivity.this);
+    }
+
+    private void comprobarArea(int fila, int columna){
+        int filas = 5;
+        int columnas = 5;
+        int conteo = 0;
+
+        int filaInicio = fila - 1;
+        int filaFin = fila + 1;
+        int columnaInicio = columna - 1;
+        int columnaFin = columna + 1;
+
+        if (fila == 0) {
+            filaInicio = 0;
+        }
+        if (fila == filas - 1) {
+            filaFin = filas - 1;
+        }
+        if (columna == 0) {
+            columnaInicio = 0;
+        }
+        if (columna == columnas - 1) {
+            columnaFin = columnas - 1;
+        }
+
+        for (int i = filaInicio; i <= filaFin; i++) {
+            for (int j = columnaInicio; j <= columnaFin; j++) {
+                if (cuadricula[i][j] == 1) {
+                    conteo++;
+                    if (conteo >=2){
+                        botones[fila][columna].setBackgroundColor(getResources().getColor(android.R.color.holo_orange_dark));
+                        return;
+                    }
+                }
+            }
+        }
+        if (conteo == 1){
+            botones[fila][columna].setBackgroundColor(getResources().getColor(android.R.color.holo_orange_light));
+        }
     }
 }
