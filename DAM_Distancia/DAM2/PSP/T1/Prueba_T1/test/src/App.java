4aeb3aa80c3b1ef85ebc0e52040034387cd6c63c
Daniel <jodamarmar@gmail.com>
2024-10-14 17:16:24 +0200

    actu

---
+++ DAM_Distancia/DAM2/PSP/T1/Prueba_T1/test/src/App.java
@@ -0,0 +1,10 @@
+package Prueba_T1.test.src;
+
+import java.lang.Process;
+import java.lang.Runtime;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
